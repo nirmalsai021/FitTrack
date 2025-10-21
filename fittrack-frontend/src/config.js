530e55f03926f43f446638f3ee0221e34f06dafd
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-21 16:15:11 +0530

    Update frontend to use correct Render backend URL

--- fittrack-frontend/src/config.js
+++ fittrack-frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL: process.env.NODE_ENV === 'production' 
-    ? 'https://fittrack-backend.onrender.com/api'
+    ? 'https://fittrack-wwr7.onrender.com/api'
     : 'http://127.0.0.1:8000/api'
 };
 
