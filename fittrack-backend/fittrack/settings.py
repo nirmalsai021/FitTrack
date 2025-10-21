a44250d3aa62b23e3cb3b9a4da91517bca3c27b2
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-22 00:53:59 +0530

    Switch to SendGrid email service

--- fittrack-backend/fittrack/settings.py
+++ fittrack-backend/fittrack/settings.py
@@ -102,11 +102,11 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
-# Email Configuration - Using environment variables
+# Email Configuration - SendGrid
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.sendgrid.net")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "apikey")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "fittrack@yourdomain.com")
