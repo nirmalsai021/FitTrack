a888f7ddb653115a835ec7ae9fdd10d9db7590c9
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-21 21:57:07 +0530

    Enable Gmail email sending

--- fittrack-backend/fittrack/settings.py
+++ fittrack-backend/fittrack/settings.py
@@ -80,4 +80,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # Email
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'birthdaywisher2025@gmail.com'
+EMAIL_HOST_PASSWORD = 'cvoh yvrg epbl laiw'
+DEFAULT_FROM_EMAIL = 'birthdaywisher2025@gmail.com'
+EMAIL_TIMEOUT = 10
