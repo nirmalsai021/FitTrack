723ae92eccba54d4c7d226222041ebdfd722803e
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-21 20:05:38 +0530

    Fix: Make workouts user-specific

--- fittrack-backend/workouts/views.py
+++ fittrack-backend/workouts/views.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets
-from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 from .models import Workout
 from .serializers import WorkoutSerializer
 
 class WorkoutViewSet(viewsets.ModelViewSet):
     serializer_class = WorkoutSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
-        return Workout.objects.all().order_by('-created_at')
+        return Workout.objects.filter(user=self.request.user).order_by('-created_at')
     
     def perform_create(self, serializer):
-        user, created = User.objects.get_or_create(username='demo_user')
-        serializer.save(user=user)
+        serializer.save(user=self.request.user)
