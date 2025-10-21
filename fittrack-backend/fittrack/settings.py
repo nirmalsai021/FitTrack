a3016e37799cf5ac294862a419957adafbf8f039
Venkat nirmal sai <venket.d.v@gmail.com>
2025-10-21 23:57:39 +0530

    Use environment variables for email

--- fittrack-backend/fittrack/settings.py
+++ fittrack-backend/fittrack/settings.py
@@ -78,11 +78,11 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
-# Email - Simple Gmail setup
+# Email - Using environment variables
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'birthdaywisher2025@gmail.com'
-EMAIL_HOST_PASSWORD = 'cvoh yvrg epbl laiw'
-DEFAULT_FROM_EMAIL = 'birthdaywisher2025@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'birthdaywisher2025@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'cvoh yvrg epbl laiw')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'birthdaywisher2025@gmail.com')
